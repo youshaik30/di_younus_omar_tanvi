33c7516aafd3ac1f4726580ecabbcb70724fd112
shaik <younushaik1@gmail.com>
2026-06-30 00:44:32 +0200

    remove all the files

--- src/main/java/de/di/similarity_measures/SimilarityMeasure.java
+++
@@ -1,8 +0,0 @@
-package de.di.similarity_measures;
-
-public interface SimilarityMeasure {
-
-    double calculate(final String string1, final String string2);
-
-    double calculate(final String[] strings1, final String[] strings2);
-}
